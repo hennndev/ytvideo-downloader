68bde6d60dfc8e60dc1eea97e5583d64952602a1
hennndev <hendraputra230897@gmail.com>
2022-08-21 22:56:23 +0700

    bug-fixed

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const express = require('express')
+const cors = require('cors')
 const appRoutes = require('./routes/appRoutes')
 
 const app = express()
 
-
 app.set('view engine', 'ejs')
+app.use(cors())
 app.use(appRoutes)
 
 app.listen(process.env.PORT || 5000, () => console.log('Server has been connected'))
--- routes/appRoutes.js
+++ routes/appRoutes.js
@@ -1,7 +1,17 @@
 const router = require('express').Router()
+const ytdl = require('ytdl-core')
+const fs = require('fs')
 
 router.get('/', async(req, res) => {
     res.render('home')
+})
+router.get(`/download`, async(req, res) => {
+    const URL = req.query.URL
+    res.header('Content-Disposition', 'attachment; filename="video.mp4')
+
+    ytdl(URL, {
+        format: 'mp4'
+    }).pipe(res)
 })
 
 module.exports = router
